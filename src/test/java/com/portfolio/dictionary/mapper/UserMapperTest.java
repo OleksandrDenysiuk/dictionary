0526cbf760d5f64b8e39421b8b219e169b3a3053
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-15 21:32:28 +0300

    Add UserMapper test

---
+++ src/test/java/com/portfolio/dictionary/mapper/UserMapperTest.java
@@ -0,0 +1,32 @@
+package com.portfolio.dictionary.mapper;
+
+import com.portfolio.dictionary.dto.UserDto;
+import com.portfolio.dictionary.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class UserMapperTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toEntity() {
+
+        UserDto userDto = new UserDto( "test", "test@test.com", "5", "5");
+
+        //when
+        User user = UserMapper.INSTANCE.toEntity( userDto );
+
+        assertNotNull( user );
+
+        assertEquals("test",user.getUsername());
+        assertEquals("test@test.com",user.getEmail());
+        assertEquals("5",user.getPassword());
+    }
+}
