89c8675e35ae0c3b084f7c68c546ba348117b22c
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-15 22:36:07 +0300

    Init sets when null

--- src/main/java/com/portfolio/dictionary/model/User.java
+++ src/main/java/com/portfolio/dictionary/model/User.java
@@ -28,13 +28,35 @@ public class User{
     private boolean active;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles;
 
     @OneToMany(cascade = CascadeType.ALL,
                 mappedBy = "user")
-    private Set<Category> categories = new HashSet<>();
+    private Set<Category> categories;
 
     @OneToMany(cascade = CascadeType.ALL,
     mappedBy = "user")
-    private Set<Test> tests = new HashSet<>();
+    private Set<Test> tests;
+
+    public Set<Role> getRoles() {
+        if(roles == null){
+            this.roles = new HashSet<>();
+        }
+        return roles;
+    }
+
+    public Set<Category> getCategories() {
+        if(categories == null){
+            this.categories = new HashSet<>();
+        }
+        return categories;
+    }
+
+    public Set<Test> getTests() {
+        if(tests == null){
+            this.tests = new HashSet<>();
+        }
+        return tests;
+    }
+
 }
