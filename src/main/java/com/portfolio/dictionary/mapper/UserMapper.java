41e54cedc7de087daf8535e0781153b60371988e
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-15 21:30:39 +0300

    Add UserMapper

---
+++ src/main/java/com/portfolio/dictionary/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.portfolio.dictionary.mapper;
+
+import com.portfolio.dictionary.dto.UserDto;
+import com.portfolio.dictionary.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toEntity(UserDto userDto);
+}
