aca6e3b7b0e5203b1b41ce3d5c2d4b466d164d7e
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-15 21:07:46 +0300

    Add UserDto

---
+++ src/main/java/com/portfolio/dictionary/dto/UserDto.java
@@ -0,0 +1,30 @@
+package com.portfolio.dictionary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    private String passwordConfirm;
+}
