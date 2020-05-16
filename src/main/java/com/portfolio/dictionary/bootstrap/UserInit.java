9f86e79c046440c0681f986c6a210131af26b010
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-16 12:37:01 +0300

    Add admin init

---
+++ src/main/java/com/portfolio/dictionary/bootstrap/UserInit.java
@@ -0,0 +1,39 @@
+package com.portfolio.dictionary.bootstrap;
+
+
+import com.portfolio.dictionary.model.User;
+import com.portfolio.dictionary.repository.RoleRepository;
+import com.portfolio.dictionary.repository.UserRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class UserInit implements ApplicationListener<ContextRefreshedEvent> {
+    private  final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder cryptPasswordEncoder;
+
+    public UserInit(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder cryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.cryptPasswordEncoder = cryptPasswordEncoder;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        User admin = User.builder()
+                .id(1L)
+                .username("admin")
+                .email("admin@admin.com")
+                .password(cryptPasswordEncoder.encode("1"))
+                .active(true)
+                .roles(Collections.singleton(roleRepository.findByName("ADMIN")))
+                .build();
+
+        userRepository.save(admin);
+    }
+}
