0142dc07dc31e7801e5f12114d775441284ce7e6
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-16 17:46:07 +0300

    Add AccountDetails class

--- src/main/java/com/portfolio/dictionary/service/UserDetailsServiceImpl.java
+++ src/main/java/com/portfolio/dictionary/service/UserDetailsServiceImpl.java
@@ -1,18 +1,17 @@
 package com.portfolio.dictionary.service;
 
+import com.portfolio.dictionary.model.AccountDetails;
 import com.portfolio.dictionary.model.Role;
 import com.portfolio.dictionary.model.User;
 import com.portfolio.dictionary.repository.UserRepository;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -25,18 +24,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public AccountDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException(username);
-        return new org.springframework.security.core.userdetails
-                .User(user.getUsername(),
+        return new AccountDetails(user.getUsername(),
                 user.getPassword(),
-                user.isActive(), true, true, true,
+                user.isActive(),
                 getAuthorities(user.getRoles()));
     }
 
-    private static List<GrantedAuthority> getAuthorities(Set<Role> roles) {
-        List<GrantedAuthority> authorities = new ArrayList<>();
+    private static Set<GrantedAuthority> getAuthorities(Set<Role> roles) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
         for (Role role : roles) {
             authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
         }
