2d5442220679f91b89155e6a22d35338a8ffbe34
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-20 22:51:57 +0300

    Add TestType service

---
+++ src/main/java/com/portfolio/dictionary/service/TestTypeService.java
@@ -0,0 +1,7 @@
+package com.portfolio.dictionary.service;
+
+import java.util.List;
+
+public interface TestTypeService {
+    List<String> getAll();
+}
---
+++ src/main/java/com/portfolio/dictionary/service/TestTypeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.portfolio.dictionary.service;
+
+import com.portfolio.dictionary.model.TestType;
+import com.portfolio.dictionary.repository.TestTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TestTypeServiceImpl implements TestTypeService {
+
+    private final TestTypeRepository testTypeRepository;
+
+    public TestTypeServiceImpl(TestTypeRepository testTypeRepository) {
+        this.testTypeRepository = testTypeRepository;
+    }
+
+    @Override
+    public List<String> getAll() {
+        List<String> types = new ArrayList<>();
+        for(TestType type : testTypeRepository.findAll()){
+            types.add(type.getTypeName());
+        }
+        return types;
+    }
+}
