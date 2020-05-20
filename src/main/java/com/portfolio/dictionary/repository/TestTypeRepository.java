f860e495e739421aa15da96726c85f16b5f4b17f
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-20 22:50:58 +0300

    Add TestType repository

---
+++ src/main/java/com/portfolio/dictionary/repository/TestTypeRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.dictionary.repository;
+
+import com.portfolio.dictionary.model.TestType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestTypeRepository extends JpaRepository<TestType, Long> {
+}
