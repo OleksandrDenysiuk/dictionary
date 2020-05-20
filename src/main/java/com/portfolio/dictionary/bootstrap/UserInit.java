92e385e49d56e3e7fdee2a43ab0bd93affe5585f
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-20 22:54:40 +0300

    Add initialization of 4 words, 1 category

--- src/main/java/com/portfolio/dictionary/bootstrap/UserInit.java
+++ src/main/java/com/portfolio/dictionary/bootstrap/UserInit.java
@@ -4,8 +4,10 @@ package com.portfolio.dictionary.bootstrap;
 import com.portfolio.dictionary.model.Category;
 import com.portfolio.dictionary.model.User;
 import com.portfolio.dictionary.model.Word;
+import com.portfolio.dictionary.repository.CategoryRepository;
 import com.portfolio.dictionary.repository.RoleRepository;
 import com.portfolio.dictionary.repository.UserRepository;
+import com.portfolio.dictionary.repository.WordRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,11 +18,15 @@ import java.util.HashSet;
 
 @Component
 public class UserInit implements ApplicationListener<ContextRefreshedEvent> {
-    private  final UserRepository userRepository;
+    private final WordRepository wordRepository;
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder cryptPasswordEncoder;
 
-    public UserInit(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder cryptPasswordEncoder) {
+    public UserInit(WordRepository wordRepository, CategoryRepository categoryRepository, UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder cryptPasswordEncoder) {
+        this.wordRepository = wordRepository;
+        this.categoryRepository = categoryRepository;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.cryptPasswordEncoder = cryptPasswordEncoder;
@@ -57,5 +63,48 @@ public class UserInit implements ApplicationListener<ContextRefreshedEvent> {
         category.setUser(admin);
 
         userRepository.save(admin);
+
+        Word word2 = Word.builder()
+                .id(2L)
+                .word("word2")
+                .translation("translation2")
+                .category(category)
+                .build();
+        wordRepository.save(word2);
+
+        Word word3 = Word.builder()
+                .id(3L)
+                .word("word3")
+                .translation("translation3")
+                .category(category)
+                .build();
+        wordRepository.save(word3);
+
+        Category category2 = Category.builder()
+                .id(2L)
+                .name("Category2")
+                .words(new HashSet<>())
+                .user(admin)
+                .build();
+
+        categoryRepository.save(category2);
+
+        Word word4 = Word.builder()
+                .id(4L)
+                .word("word4")
+                .translation("translation4")
+                .category(category2)
+                .build();
+        wordRepository.save(word4);
+
+        Word word5 = Word.builder()
+                .id(5L)
+                .word("word5")
+                .translation("translation5")
+                .category(category2)
+                .build();
+        wordRepository.save(word5);
+
+
     }
 }
