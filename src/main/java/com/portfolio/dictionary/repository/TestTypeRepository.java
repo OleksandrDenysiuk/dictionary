b51fcadaee64cadb1f3d3e33d5a6e6b7bdd68480
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-21 23:08:48 +0300

    Add findByName method

--- src/main/java/com/portfolio/dictionary/repository/TestTypeRepository.java
+++ src/main/java/com/portfolio/dictionary/repository/TestTypeRepository.java
@@ -4,4 +4,5 @@ import com.portfolio.dictionary.model.TestType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestTypeRepository extends JpaRepository<TestType, Long> {
+    TestType findByTypeName(String name);
 }
