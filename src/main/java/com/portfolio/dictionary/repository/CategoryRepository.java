eb4a68b56506dd1e6880eb326df60aff65efcac0
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-13 10:00:50 +0200

    Add method

--- src/main/java/com/portfolio/dictionary/repository/CategoryRepository.java
+++ src/main/java/com/portfolio/dictionary/repository/CategoryRepository.java
@@ -3,6 +3,10 @@ package com.portfolio.dictionary.repository;
 import com.portfolio.dictionary.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     Category findByIdAndUserId(Long categoryId, Long userId);
+    Set<Category> findAllByUserId(Long userId);
+
 }
