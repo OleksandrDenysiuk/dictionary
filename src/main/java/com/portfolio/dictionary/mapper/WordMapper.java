3e6fa72378bd3fcad408fb29d8f9413f0571f603
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-13 10:01:12 +0200

    Fix bug

--- src/main/java/com/portfolio/dictionary/mapper/WordMapper.java
+++ src/main/java/com/portfolio/dictionary/mapper/WordMapper.java
@@ -3,6 +3,7 @@ package com.portfolio.dictionary.mapper;
 import com.portfolio.dictionary.dto.WordDto;
 import com.portfolio.dictionary.model.Word;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,6 @@ public interface WordMapper {
 
     WordMapper INSTANCE = Mappers.getMapper(WordMapper.class);
 
+    @Mapping(source = "category.id", target = "categoryId")
     WordDto toDto(Word word);
 }
