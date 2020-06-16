a5173aca22f387f151c40b37403ed25a34873efa
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-16 23:13:12 +0200

    Add Result repository

---
+++ src/main/java/com/portfolio/dictionary/repository/ResultRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.dictionary.repository;
+
+import com.portfolio.dictionary.model.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+}
