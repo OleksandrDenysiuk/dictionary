83efc693b298512332bfbdbdfe4997ece2c7b569
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-16 23:16:40 +0200

    Add session storage

--- src/main/resources/static/js/test/script.js
+++ src/main/resources/static/js/test/script.js
@@ -1,21 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form'),
-        submitBtn = document.querySelector('.container form button');
+        testTypeSelect = document.querySelector('form select');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const formData = new FormData(form);
         const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
-        console.log(checkboxes);
         let boxes = [];
         checkboxes.forEach(box => {
             boxes.push(box.value);
         });
-        formData.append('test', JSON.stringify(boxes));
 
-        const jsonData = JSON.stringify(Object.fromEntries(formData.entries()));
+        sessionStorage.setItem('type', testTypeSelect.value);
+        sessionStorage.setItem('categories', JSON.stringify(boxes));
+        sessionStorage.setItem('startTime', JSON.stringify(new Date()));
 
-        console.log(jsonData);
+        form.submit();
     });
 });
